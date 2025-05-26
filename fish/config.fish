724274cc0168a8d84eee26e0c8a8d271fec299fe
Keyvhinng <keyvhinng@gmail.com>
2025-05-25 20:41:31 -0500

    add local bin for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
-    fish_add_path /Users/ <user >/.codeium/windsurf/bin
+    fish_add_path /Users/user/.local/bin
+    fish_add_path /Users/user/.codeium/windsurf/bin
     fzf --fish | source
     starship init fish | source
 end
